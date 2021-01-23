c06204db5d46bf3e638bda569afa2a732774d3b0
Jared Anders Isaksen <rayos.dev@gmail.com>
2021-01-23 17:44:08 +0100

    Project Setup

---
+++ src/views/Game.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Game">
+    <h1>This is the Game</h1>
+    <div id="facetest">
+      <button v-on:click="show = !show">
+        Toggle Fade
+      </button>
+      <transition name="fade">
+        <p v-if="show">transition</p>
+      </transition>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+<style lang="scss">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ src/views/Start.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="start">
+    <img alt="Game logo" src="../assets/KaaM-logo.svg">
+    <Message msg="Kaa Math game"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Message from '@/components/Message.vue'
+
+export default {
+  name: 'Start',
+  components: {
+    Message
+  }
+}
+</script>
